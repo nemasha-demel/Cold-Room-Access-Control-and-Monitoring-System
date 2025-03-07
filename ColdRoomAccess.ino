ccbd63df60d33d2d6c568b22d10f8fe80c18e820
nemasha-demel <nemashahirushidemel@gmail.com>
2025-03-07 08:26:31 +0530

    C code
    Arduino

---
+++ ColdRoomAccess.ino
@@ -0,0 +1,101 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <ESP32Servo.h>
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+const char* ssid = "-----------"; // Wifi Name SSID
+const char* password = "----------"; // WIFI Password
+
+void startCameraServer();
+
+boolean matchFace = false;
+boolean activateServo = false;
+long prevMillis = 0;
+int interval = 5000;
+
+#define ServoPin 2 // Define the servo control pin
+Servo myServo;
+
+void setup() {
+  myServo.attach(ServoPin);
+  myServo.write(0); // Set initial position
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);
+    s->set_brightness(s, 1);
+    s->set_saturation(s, -2);
+  }
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  if(matchFace == true && activateServo == false) {
+    activateServo = true;
+    myServo.write(90); // Move servo to unlock position
+    prevMillis = millis();
+  }
+
+  if (activateServo == true && millis() - prevMillis > interval) {
+    activateServo = false;
+    matchFace = false;
+    myServo.write(0); // Move servo back to lock position
+  }
+}
